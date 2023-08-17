10e1f21bcbcd11e97d9332247bbf20590085080b
Alona1212 <139923603+Alona1212@users.noreply.github.com>
2023-08-18 00:57:27 +0200

    Add files via upload
    hw

---
+++ script09.js
@@ -0,0 +1,44 @@
+
+// Написать скрипт, который находит параграфы и четным параграфам меняет текст на 'Я четный', a нечетным на 'Я нечетный'.
+// let paragraphs = document.querySelectorAll('p');
+
+// console.log(paragraphs);
+
+// for(let i = 1; i<paragraphs.length; i++){
+//     if(i%2==1){
+//         paragraphs[i].innerText = 'Я четный';
+//     }
+// }
+
+// for(let i = 1; i<paragraphs.length; i+=2){
+ //     paragraphs[i].innerText = 'Я четный';
+// }
+
+// Задача: Написать скрипт, который находит параграфы, которые находятся внутри div-ов и первым 2 параграфам текст меняет на указанные пользователем значения.
+//     let paragraphs = document.querySelectorAll('p');  
+//     for (let i = paragraphs.length-1; i >= paragraphs.length-2; i--){
+//     paragraphs[i].innerText = prompt("text");
+//        }
+
+
+
+// Задача: Написать скрипт, который находит параграфы с классом 'p1' и выводит (console) общее содержимое параграфов.
+// let classparagraph = document.querySelectorAll('.p1');
+
+// console.log(classparagraph);
+
+
+
+// Задача: Написать скрипт, который находит параграфы и последним 2 четным параграфам меняет текст на указанные пользователем значения.
+// let paragraphs = document.querySelectorAll('p');
+
+// for (let i = 0; i < paragraphs.length; i++){
+//      if (i+=2) {paragraphs[i].innerText = prompt'text'};   
+
+
+// Задача: Написать скрипт, который находит параграфы и выводит (console) текстовое содержимое начная с 3 параграфа до 6.
+// let paragraphs = document.querySelectorAll('p');
+// for (let i = 3; i>=6; i++){
+// console.log(paragraphs[0]);
+
+    
